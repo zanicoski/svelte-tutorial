a9445da27e99fefd89b7a9e5f0f00a84bf8eef6d
Bruna Zanicoski <bruna.zanicoski@gmail.com>
2022-08-09 18:39:05 -0300

    Added Delete Task feature

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,12 +1,67 @@
+<script>
+    import {Template} from 'svelte-native/components'
+   
+    let todos = []
+    
+    let dones = [] //completed items go here
+		const removeFromList = (list, item) => list.filter(t => t !== item);
+		const addToList = (list, item) => [item, ...list]
+
+    let textFieldValue = ""
+    const { default: App } = require("~/App.svelte");
+
+    async function onItemTap(args) {
+		let result = await action("What do you want to do with this task?", "Cancel", [
+				"Mark completed",
+				"Delete forever"
+		]);
+
+		console.log(result); // Logs the selected option for debugging.
+		let item = todos[args.index]
+		switch (result) {
+				case "Mark completed":
+						dones = addToList(dones, item) // Places the tapped active task at the top of the completed tasks.
+						todos = removeFromList(todos, item) // Removes the tapped active task.
+						break;
+				case "Delete forever":
+						todos = removeFromList(todos, item) // Removes the tapped active task.
+						break;
+				case "Cancel" || undefined: // Dismisses the dialog
+						break;
+		}
+	}
+
+    function onButtonTap() {
+        if (textFieldValue === "") return;
+        console.log("New task added: " + textFieldValue + ".");
+            todos = [{ name: textFieldValue }, ...todos]
+            textFieldValue = "";
+    }
+</script>
 <page>
     <actionBar title="My To do list" />
 
     <tabView>
             <tabViewItem title="To Do">
-                    <label textWrap="true">This tab will list active tasks and will let users add new tasks.</label>
+                    <gridLayout columns="*,120" rows="70,*">
+                        <textField col="0" row="0" bind:text="{textFieldValue}" hint="Type new task..." editable="true" on:returnPress="{onButtonTap}" />
+                        <button col="1" row="0" text="Add task" on:tap="{onButtonTap}" />
+
+                        <listView items="{todos}" on:itemTap="{onItemTap}" row="1" colSpan="2">
+                            <Template let:item>
+                                <label text="{item.name}" textWrap="true" />
+                            </Template>
+                        
+                        </listView>
+                    </gridLayout>
             </tabViewItem>
             <tabViewItem title="Completed">
-                    <label textWrap="true">This tab will list completed tasks for tracking.</label>
+                <listView items="{dones}">
+                    <Template let:item>
+                        <label text="{item.name}" textWrap="true" />
+                    </Template>
+                
+                </listView>
             </tabViewItem>
     </tabView>
 </page>
